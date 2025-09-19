bda10f897b9daac6e533a1e406370515bbffbc38
Gabriel Amoêdo <gbas95@gmail.com>
2025-09-19 13:31:55 -0400

    MainActivity

--- app/src/main/java/com/ifam/app/MainActivity.kt
+++ app/src/main/java/com/ifam/app/MainActivity.kt
@@ -1,29 +1,42 @@
-package com.ifam.app
+package com.ifam.app01
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ifam.app.ui.theme.AppTheme
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme{
+                Surface {
+                    HelloScreen()
                 }
             }
         }
@@ -31,17 +44,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HelloScreen(){
+
+    //estado que vive durante a composição desta tela
+
+    var count by rememberSaveable { mutableIntStateOf(0) } //cria um estado observável que pode ser modificado
+    Column(
+        modifier = Modifier.fillMaxSize().padding(all = 24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Contador: $count")
+        Spacer(Modifier.height(height = 16.dp)) //distanciamento entre um componente e o outro
+        Button(onClick = { count++}) {
+            Text(text = "somar +1")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
-    }
+private fun HelloPreview(){
+    MaterialTheme {HelloScreen()}
 }
+
+
+//statehosting
